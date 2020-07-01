20f0c0728fffb21e51ebe1a66cdffe281c4ef21a
Dell <Dell@DESKTOP-ESENG68>
2020-07-01 16:11:02 +0530

    pyramids1

---
+++ pyramids1/pyramids1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyramids1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.WriteLine("enter the numer of rows: ");
+            rows=Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows;++i)
+            {
+                for (j = 1; j <= i; ++j) 
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
