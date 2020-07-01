b7ae6dada697ce8ddbe724bbadef0698cc379e32
Dell <Dell@DESKTOP-ESENG68>
2020-07-01 16:44:18 +0530

    pattern1

--- Pattern2/Pattern2/Program.cs
+++ Pattern2/Pattern2/Program.cs
@@ -10,6 +10,26 @@ namespace Pattern2
     {
         static void Main(string[] args)
         {
+            int i,j,rows,k=0;
+            Console.WriteLine("enter num of rows:");
+            rows=Convert.ToInt32(Console .ReadLine());
+              for(i=1;i<=rows;++i,k=0)
+            {
+                for (j = 1; j <= rows - i;++j)
+                {
+                    Console.WriteLine(" ");
+          
+                }
+                while(k!=2*i-1)
+                {
+                    Console.WriteLine("*");
+                    ++k;
+
+                }
+                Console.ReadLine();
+            }
+            
+
         }
     }
 }
