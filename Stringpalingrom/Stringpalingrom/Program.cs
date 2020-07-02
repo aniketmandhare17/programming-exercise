1d985e090cc5bb2c09207f0a3a80409145feafdf
Dell <Dell@DESKTOP-ESENG68>
2020-07-03 00:15:32 +0530

    Cheak the Number is Palindrom or Not

---
+++ Stringpalingrom/Stringpalingrom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringpalingrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1, r="";
+            Console.WriteLine("enter the string:");
+            s1 = (Console.ReadLine());
+            for(int i=s1.Length -1;i>=0;i--)
+            {
+                r += s1[i].ToString();
+            }
+            if(r==s1)
+            {
+                Console.WriteLine("string is Palindrom");
+
+            }
+            else
+            {
+                Console.WriteLine("string is not Palindrom");
+            }
+            Console.ReadLine();
+        }
+    }
+}
