19d59367b905e35840afaea766cb85305ff4d5eb
Dell <Dell@DESKTOP-ESENG68>
2020-07-05 14:52:25 +0530

    WAP List Program To Accept The Collection Of Student Name List And Display It

---
+++ ListProgram/ListProgram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Student = new List<string>();
+            Student.Add("Aniket");
+            Student.Add("Roshan");
+            Student.Add("Pooja");
+            Student.Add("Shree");
+            Student.Add("piu");
+            Student.Add("Ankita");
+            Student.Add("Aniket");
+            Student.Add("Ritika");
+
+            foreach (var studentname in Student )
+            {
+                Console.WriteLine(studentname);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
