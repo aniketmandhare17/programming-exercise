35e956e4952f96a7252fe1b37bc259c716045233
Dell <Dell@DESKTOP-ESENG68>
2020-07-05 15:32:41 +0530

    WAP To List of Student Name And Perform Add, Fetch, Remove, Find Operation On Student Name List

--- ListProgram/ListProgram/Program.cs
+++ ListProgram/ListProgram/Program.cs
@@ -15,15 +15,41 @@ namespace ListProgram
             Student.Add("Roshan");
             Student.Add("Pooja");
             Student.Add("Shree");
-            Student.Add("piu");
+            Student.Add("Piu");
             Student.Add("Ankita");
             Student.Add("Aniket");
             Student.Add("Ritika");
 
+            Console.WriteLine("Collection Student Name in Student List : ");
+
             foreach (var studentname in Student )
             {
                 Console.WriteLine(studentname);
+                
             }
+            Console.WriteLine();
+
+            Console.WriteLine("Remove operation on Student List : ");
+
+            Console.WriteLine("\nRemove Student Name At Position (3)");
+           
+            Student.RemoveAt(3);
+
+            Console.WriteLine();
+            Console.WriteLine("After Removing Collection Student Name in Student List : ");
+            foreach (var studentname in Student)
+            {
+                Console.WriteLine(studentname);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Search Student Name in Student List: ");
+
+            Console.WriteLine("\nFind: Student where name contains \"Piu\": {0}",
+                
+            Student.Find(x => x.Contains("Piu")));
+
             Console.ReadLine();
 
         }
