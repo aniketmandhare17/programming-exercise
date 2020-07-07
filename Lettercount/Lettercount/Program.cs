22f9b51f87aa30edeaa2f1683bbdf29f2cfd830a
Dell <Dell@DESKTOP-ESENG68>
2020-07-07 16:27:51 +0530

    WAP to Accept the String And Display number of character Occurrences of character

---
+++ Lettercount/Lettercount/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lettercount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str;
+            int i;
+            Console.WriteLine("Enter The String");
+           str=(Console.ReadLine());
+
+            while(str.Length >0)
+            {
+                int Count = 0;
+                Console.WriteLine($"{str[0]}=>");
+                for (i = 0; i < str.Length; i++) 
+                {
+                    if(str[0]==str[i])
+                    {
+                        Count++;
+
+                    }
+                }
+                Console.WriteLine(Count );
+               str= str.Replace(str[0].ToString(),string.Empty);
+
+            }
+           
+ Console.ReadLine();
+           
+        }
+    }
+}
