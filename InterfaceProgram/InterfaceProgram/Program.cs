2953b7ddc8956e4e65084cbefc52c651da3417e3
Dell <Dell@DESKTOP-ESENG68>
2020-07-07 13:07:40 +0530

    LINQ Program to declare Array and display even num in that Array.

---
+++ InterfaceProgram/InterfaceProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProgram
+{
+   public  interface Car
+    {
+         void Accept();
+         void Display();
+    }
+    class Honda: Car
+    {
+        int ID;
+        String name;
+        public void Accept()
+        {
+            
+            Console.WriteLine("Enter the Car ID:");
+            ID = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Enter the Car Name:");
+            name =(Console.ReadLine());
+        }
+        public void Display()
+        {
+            Console.WriteLine("Car ID is :"+ID);
+            Console.WriteLine("Car Name is :"+name);
+        }
+       
+        static void Main(string[] args)
+        {
+            Honda C = new Honda();
+            C.Accept();
+            C.Display();
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ LINQProgMarks/LINQProgMarks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProgMarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Marks = { 23, 56, 78, 34, 12 };
+            IEnumerable<int> result = from p1 in Marks
+                                      where p1 % 2 == 0
+                                      select p1;
+            Console.WriteLine("Even Number in Above Array is : ");
+            foreach (int data in result)
+            {
+                 Console.WriteLine(data );
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
