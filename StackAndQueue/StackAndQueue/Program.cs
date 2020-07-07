a4b8e7f32804987cea1e2d9780a953b6c1478bfa
aniketmandhare17 <aniketmandhare17@gmail.com>
2020-07-07 22:37:15 +0530

    WAP Perform Operation on  Stack and Queue

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Q = new Queue();
+            for (int i = 0; i < 5; i++)
+            {
+                Q.Enqueue(i + 1);
+
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(Q.Dequeue());
+                
+
+
+            }
+
+                /*Stack s = new Stack();
+                for (int i=0;i<5;i++)
+                {
+                    s.Push(i);
+
+
+                }
+                for(int i=0;i<5;i++)
+                {
+                    Console.WriteLine(s.Pop());
+                }*/
+                Console.ReadLine();
+        }
+    }
+}
