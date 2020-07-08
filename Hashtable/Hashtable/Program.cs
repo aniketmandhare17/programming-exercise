c134813ff4129953b6878ae55f848895de1f6dee
aniketmandhare17 <aniketmandhare17@gmail.com>
2020-07-08 13:08:41 +0530

    WAP Hashtable Demo Peogram.

---
+++ Hashtable/Hashtable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable H = new Hashtable ();
+            H.Add("Name", "Aninet");
+            H.Add("Add", "Pune");
+            H.Add("Age", "22");
+           
+            Console.WriteLine(H["Name"]);
+
+            Console.WriteLine(H["Name"]);
+            Console.ReadLine();
+
+
+        }
+    }
+}
